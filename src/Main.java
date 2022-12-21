18ae6d388a774ef511a1233f7a9d2c4bacac83fa
hanjo <eeriehan@google.com>
2022-12-20 19:47:51 -0500

    added the feature planet1

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,16 @@ public class Main {
         Fxron.color="green";
         Fxron.size="4382742km";
         Fxron.temp="-23478274372k";
+
+
+
+        //-----------added on FeaturePlant1
+
+        Planet1 stuart = new Planet1();
+        stuart.color = "white";
+        stuart.size = 123132;
+        stuart.rings = "5";
+        stuart.temperature = "-12618762";
+
     }
 }
